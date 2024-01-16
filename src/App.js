2499bcadddf4f14327a1f909af8be77d9a5a3b44
shailendra08 <shailendra.jain0894@gmail.com>
2024-01-16 18:00:34 -0500

    Accordian 16/01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Textfrom from "./components/Textfrom";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
       <div className="container">
-      <Textfrom heading="Enter the text to analyze below"/>
+      <About/>
       </div>
     </>
   );
--- src/components/Textfrom.js
+++ src/components/Textfrom.js
@@ -1,36 +1,34 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export default function Textfrom(props) {
-    const handleUpClick = () =>{
-        let newText = text.toUpperCase();
-        setText(newText)
-    }
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleLowClick = () =>{
-        let newText = text.toLowerCase();
-        setText(newText)
-    } 
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const clearClick = () =>{
-        let newText = "";
-        setText(newText)
-    }
+  const clearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
 
-    const handleTitleClick = () =>{
-        let newText = text.split(' ')
-        .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
-        .join(' ');
-        setText(newText)
-        
-    }
-    const handleOnChange = (event) =>{
-        setText(event.target.value)
-    }
+  const handleTitleClick = () => {
+    let newText = text
+      .split(" ")
+      .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+      .join(" ");
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
-
-    const [text, setText] = useState('');
-    return (
+  const [text, setText] = useState("");
+  return (
     <>
       <div className="container">
         <h1>{props.heading}</h1>
@@ -41,15 +39,25 @@ export default function Textfrom(props) {
           value={text}
           onChange={handleOnChange}
         ></textarea>
-        <br/>
-        <button className="btn btn-primary mx-1" onClick={handleTitleClick}>Convert To Titlecase</button>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert To Lowercase</button> 
-        <button className="btn btn-primary mx-1" onClick={clearClick}>Clear Text</button> 
+        <br />
+        <button className="btn btn-primary mx-1" onClick={handleTitleClick}>
+          Convert To Titlecase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert To Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Convert To Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={clearClick}>
+          Clear Text
+        </button>
       </div>
       <div className="container">
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} Words and {text.length} Characters </p>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters{" "}
+        </p>
         <p>{0.008 * text.split(" ").length} Minutes of reading </p>
         <h3>Preview</h3>
         <p>{text}</p>

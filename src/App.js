a83c87a986b9307e467624f178491087a671fe88
shailendra08 <shailendra.jain0894@gmail.com>
2024-01-17 09:53:38 -0500

    Dark Mode App In About Us Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+// import Textfrom from "./components/Textfrom"
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
+
       <div className="container">
+      {/* <Textfrom heading="Enter the text below to analyze"/> */}
       <About/>
       </div>
     </>
--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,95 @@
-import Accordion from 'react-bootstrap/Accordion';
-import React from "react";
+import React, { useState } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export default function About() {
-  
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  const [btnStyle, setBtnStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+    border: '1px solid black'
+  });
+
+  const [btnTxt, setBtnTxt] = useState ("Enable Dark Mode")
+
+  let toggleStyle = () => {
+    if (myStyle.color === "black") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      });
+      setBtnStyle({
+        color: "white",
+        backgroundColor: "black",
+        border: '1px solid white'
+      });
+      setBtnTxt("Enable Light Mode")
+    } else {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      setBtnStyle({
+        color: "black",
+        backgroundColor: "white",
+        border: '1px solid black'
+      });
+      setBtnTxt("Enable Dark Mode");
+    }
+  };
+
   return (
-    <>
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-    </>
+    <div className="container" style={myStyle}>
+      <h1>About US</h1>
+      <Accordion defaultActiveKey="0" style={myStyle}>
+        <Accordion.Item eventKey="0" style={myStyle}>
+          <Accordion.Header style={myStyle}>Accordion Item #1</Accordion.Header>
+          <Accordion.Body style={myStyle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1" style={myStyle}>
+          <Accordion.Header style={myStyle}>Accordion Item #2</Accordion.Header>
+          <Accordion.Body style={myStyle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2" style={myStyle}>
+          <Accordion.Header style={myStyle}>Accordion Item #3</Accordion.Header>
+          <Accordion.Body style={myStyle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br />
+      <ButtonGroup size="lg" className="mb-2">
+        <Button style={btnStyle} onClick={toggleStyle}>
+          {btnTxt}
+        </Button>
+      </ButtonGroup>
+    </div>
   );
 }

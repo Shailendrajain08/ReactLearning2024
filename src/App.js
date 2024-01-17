2a036df146973602f8d0eff3ae5dccc3efa63b56
shailendra08 <shailendra.jain0894@gmail.com>
2024-01-17 15:32:11 -0500

    Text Utils with Router and Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textfrom from "./components/Textfrom";
 import Alert from "./components/Alert";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("dark"); // Weather Dark mode is enabled or not
   const [alert, setAlert] = useState({
     message: null,
-    type:null
+    type: null,
   });
 
   const showAlert = (message, type) => {
@@ -17,7 +18,6 @@ function App() {
       message: message,
       type: type,
     });
-    
   };
 
   let toggleMode = (setAlert) => {
@@ -25,21 +25,38 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042845";
       showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
   };
 
   return (
     <>
-      <Navbar mode={mode} title="TextUtils" toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container">
-        <Textfrom showAlert={showAlert} heading="Enter the text below to analyze" mode={mode}/>
-        {/* <About /> */}
-      </div>
+      {/* <Textfrom showAlert={showAlert} heading="Enter the text below to analyze" mode={mode}/> */}
+      {/* <About /> */}
+
+      <BrowserRouter>
+        <Navbar mode={mode} title="TextUtils" toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <Textfrom
+                showAlert={showAlert}
+                heading="Enter the text below to analyze"
+                mode={mode}
+              />
+            }
+          />
+          <Route path="about" exact element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
